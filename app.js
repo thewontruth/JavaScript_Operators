12f3870e6499c56357a79a0148e0ddaa8cf635c1
thewontruth <thewontruth@gmail.com>
2023-06-14 12:04:49 -0500

    Java Operators

--- app.js
+++ app.js
@@ -4,16 +4,61 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+
+let a = 20;
+let b = 4;
+
+let sum = a + b;  
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 
 // YOUR CODE HERE
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isAwake);
+console.log("eleven" == isAwake);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+
+
+
+
+
 
 // YOUR CODE HERE
